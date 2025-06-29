ca78124ab7ba0f5e89e0a89297c76e0c03d4337a
ChrisNotDefined <dev.chrisap@gmail.com>
2025-06-28 18:26:07 -0600

    add: favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/lictenberg-sim/' : '/',
+  base: mode === 'production' ? '/lichtenberg-sim/' : '/',
 }));
